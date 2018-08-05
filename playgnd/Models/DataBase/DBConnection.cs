0534e77a22a914f886ea18c58167fae9368c4f5c
Prudchenko <33181189+Prudchenko@users.noreply.github.com>
2018-08-05 19:28:39 +0300

    Update DBConnection.cs

--- playgnd/Models/DataBase/DBConnection.cs
+++ playgnd/Models/DataBase/DBConnection.cs
@@ -12,7 +12,7 @@ namespace playgnd.Models.DataBase
 
         public DbConnection()
         {
-            Connection = new MySqlConnection("server=195.123.8.52;User ID=akeno;Password=akenomlg;Database=testDB;SslMode=none;CHARSET=utf8;");
+            Connection = new MySqlConnection("server=195.123.8.52;User ID=******;Password=*********;Database=testDB;SslMode=none;CHARSET=utf8;");
         }
 
         public void Dispose()
